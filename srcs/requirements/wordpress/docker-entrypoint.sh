e494468630959f8adbf6575752e3845b77b3ef3b
Emran BinJamaan <therealemran.design@gmail.com>
2025-08-15 11:57:31 +0400

    fix: faulty sed due to replace-all

--- srcs/requirements/wordpress/docker-entrypoint.sh
+++ srcs/requirements/wordpress/docker-entrypoint.sh
@@ -12,7 +12,7 @@ if [ ! -f /var/www/html/wordpress/wp-config-sample.php ]; then
 fi
 
 # Set the Database Credentials
-sed -i 's|DB_NAME_here|'${DB_NAME}'|g' /var/www/html/wordpress/wp-config-sample.php
+sed -i 's|database_name_here|'${DB_NAME}'|g' /var/www/html/wordpress/wp-config-sample.php
 sed -i 's|username_here|'${DB_USER}'|g' /var/www/html/wordpress/wp-config-sample.php
 sed -i 's|password_here|'${DB_PASSWORD}'|g' /var/www/html/wordpress/wp-config-sample.php
 sed -i 's|localhost|'${DB_HOST}'|g' /var/www/html/wordpress/wp-config-sample.php
